1db95eadab608e2e1789a0b74714da07d64f0c45
Mishmore <michellemore28@gmail.com>
2017-03-06 15:17:18 -0500

    print coders ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,11 +29,44 @@ var coders = [ { nombre : "Meche", apellido : "Zubieta",  promocion : "1era. pro
 			 { nombre : "Diana", apellido : "Navarro",  promocion : "5ta. promoción", estado : "Trabajando" } ];
 
 function imprimirCoders() {
-  for (var i=0; i<coders.length; i++) {
-    document.getElementById('print-coders').innerHTML += "Nombre: " + coders[i].nombre + "<p>" +
-                                                        "Apellido: " + coders[i].apellido + "<p>" +
-                                                        "Promoción: " + coders[i].promocion + "<p>" +
-                                                            "Estado: " + coders[i].estado + "<p>";
+  //for (var i=0; i<coders.length; i++) {
+    document.getElementById('meche').innerHTML += "Nombre: " + coders[0].nombre + "<p>" +
+                                                        "Apellido: " + coders[0].apellido + "<p>" +
+                                                        "Promoción: " + coders[0].promocion + "<p>" +
+                                                            "Estado: " + coders[0].estado + "<p>";
 
-  }
-} imprimirCoders();
+    document.getElementById('arabela').innerHTML += "Nombre: " + coders[1].nombre + "<p>" +
+                                                        "Apellido: " + coders[1].apellido + "<p>" +
+                                                        "Promoción: " + coders[1].promocion + "<p>" +
+                                                            "Estado: " + coders[1].estado + "<p>";
+
+    document.getElementById('maria').innerHTML += "Nombre: " + coders[2].nombre + "<p>" +
+                                                        "Apellido: " + coders[2].apellido + "<p>" +
+                                                        "Promoción: " + coders[2].promocion + "<p>" +
+                                                            "Estado: " + coders[2].estado + "<p>";
+
+    document.getElementById('guadalupe').innerHTML += "Nombre: " + coders[3].nombre + "<p>" +
+                                                        "Apellido: " + coders[3].apellido + "<p>" +
+                                                        "Promoción: " + coders[3].promocion + "<p>" +
+                                                            "Estado: " + coders[3].estado + "<p>";
+
+    document.getElementById('sara').innerHTML += "Nombre: " + coders[4].nombre + "<p>" +
+                                                "Apellido: " + coders[4].apellido + "<p>" +
+                                                "Promoción: " + coders[4].promocion + "<p>" +
+                                                    "Estado: " + coders[4].estado + "<p>";
+
+    document.getElementById('daniella').innerHTML += "Nombre: " + coders[5].nombre + "<p>" +
+                                                  "Apellido: " + coders[5].apellido + "<p>" +
+                                                  "Promoción: " + coders[5].promocion + "<p>" +
+                                                      "Estado: " + coders[5].estado + "<p>";
+
+    document.getElementById('michelle').innerHTML += "Nombre: " + coders[6].nombre + "<p>" +
+                                                  "Apellido: " + coders[6].apellido + "<p>" +
+                                                  "Promoción: " + coders[6].promocion + "<p>" +
+                                                      "Estado: " + coders[6].estado + "<p>";
+
+    document.getElementById('diana').innerHTML += "Nombre: " + coders[7].nombre + "<p>" +
+                                                  "Apellido: " + coders[7].apellido + "<p>" +
+                                                  "Promoción: " + coders[7].promocion + "<p>" +
+                                                      "Estado: " + coders[7].estado + "<p>";
+  } imprimirCoders();
